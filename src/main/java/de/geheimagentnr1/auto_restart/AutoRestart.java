c92de7ebf9376b63056b64f5343098396dbcf219
GeheimagentNr1 <geheimelache@hotmail.com>
2020-08-21 22:59:36 +0200

    Warnung bearbeitet

--- src/main/java/de/geheimagentnr1/auto_restart/AutoRestart.java
+++ src/main/java/de/geheimagentnr1/auto_restart/AutoRestart.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 
 
-@SuppressWarnings( "UtilityClassWithPublicConstructor" )
+@SuppressWarnings( { "UtilityClassWithPublicConstructor", "unused" } )
 @Mod( AutoRestart.MODID )
 public class AutoRestart {
 	
